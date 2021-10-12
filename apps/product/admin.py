900f5d513b87ea7d0abac17043f391d89c58e35a
Nelson Alejandro Fuentes Paredes <nelsonafp12@gmail.com>
2021-10-11 23:55:19 -0500

    Update admin.py

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from apps.product.models import Brand, ProductCategory
+from apps.product.models import Brand, ProductCategory, MeasurementUnit
 
 
 class BrandAdmin(admin.ModelAdmin):
@@ -40,5 +40,26 @@ class ProductCategoryAdmin(admin.ModelAdmin):
     ]
 
 
+class MeasurementUnitAdmin(admin.ModelAdmin):
+    inlines = [
+        # DocumentTypeStackedInLine
+    ]
+    list_display = [
+        'id',
+        'name',
+        'code',
+        'is_active'
+    ]
+    list_filter = [
+        'restaurant',
+        'is_active'
+    ]
+    search_fields = [
+        'name',
+        'code'
+    ]
+
+
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(ProductCategory, ProductCategoryAdmin)
+admin.site.register(MeasurementUnit, MeasurementUnitAdmin)
