be3b2ba01ca3e418086821c3ac26c7dd5e043ff3
Nelson Alejandro Fuentes Paredes <nelsonafp12@gmail.com>
2021-10-14 00:00:15 -0500

    Update serializers.py

--- apps/warehouse/serializers.py
+++ apps/warehouse/serializers.py
@@ -7,6 +7,7 @@ class WarehouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Warehouse
         fields = [
+            'id',
             'name',
             'address',
             'is_main',
