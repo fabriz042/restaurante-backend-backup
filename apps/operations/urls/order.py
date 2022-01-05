1e1493292b9700da5c62beea2d564da20eb85d21
Nelson Alejandro Fuentes Paredes <nelsonafp12@gmail.com>
2022-01-04 19:03:23 -0500

    Update order.py

--- apps/operations/urls/order.py
+++ apps/operations/urls/order.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 
-from apps.operations.views import OrderListCreateAPIView, OrderRetrieveUpdateDestroyAPIView
+from apps.operations.views import OrderListCreateAPIView, OrderRetrieveUpdateDestroyAPIView, OpenedOrderListAPIView, \
+    OrderExtendedListAPIView
 
 urlpatterns = [
     path('', OrderListCreateAPIView.as_view()),
     path('<pk>', OrderRetrieveUpdateDestroyAPIView.as_view()),
+    path('opened/', OpenedOrderListAPIView.as_view()),
+    path('extended/', OrderExtendedListAPIView.as_view()),
     path('detail/', include('apps.operations.urls.order_detail')),
 ]
