244c65e877b653ce260aabae7fed33f711e4eaf9
Michael <malvarezq@unsa.edu.pe>
2022-10-18 23:18:47 -0500

    increse correlative after close order

--- apps/operations/views.py
+++ apps/operations/views.py
@@ -570,6 +570,9 @@ class CloseOrderAPIView(generics.UpdateAPIView):
         order.save()
         order.table.state = Table.State.FREE
         order.table.save()
+        serie = Serie.objects.filter(payment_document=order.payment_document, code=order.serie).first()
+        if serie:
+            Serie.objects.filter(id=serie.id, is_active=True).update(correlative=serie.correlative+1)
         return Response(status=200)
 
 
